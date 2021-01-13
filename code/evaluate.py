98b0738f3cca301edcbb0889ddc29601875f931f
Tova Erbén <33969023+datatjej@users.noreply.github.com>
2021-01-13 02:41:08 +0100

    corrected evaluation function

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,17 +1,20 @@
 import argparse
 
-def parse_input(input_file):
-    tokens_list = []
+def parse_labels(input_file):
+    input_tokens = []
+    correct_lemmas = []
     with open(input_file) as f:
         print("Parsing input...")
         lines = f.readlines()
         for line in lines:
-            clean_line = line.rstrip("\n")
-            if clean_line:
-                token = clean_line
-                tokens_list.append(token)
-    print("No of tokens: ", len(tokens_list))
-    return tokens_list
+            clean_line = line.rstrip("\n").split("\t")
+            if len(clean_line) > 1:
+                token = clean_line[0]
+                input_tokens.append(token)
+                lemma = clean_line[1]
+                correct_lemmas.append(lemma)
+    print("No of input tokens: ", len(correct_lemmas))
+    return input_tokens, correct_lemmas
 
 def parse_output(output_file):
     output_list = []
@@ -27,13 +30,24 @@ def parse_output(output_file):
     print("No of normalized tokens: ", len(output_list))
     return output_list
 
-def evaluate(input_list, output_list):
+def evaluate(token_list, label_list, output_list):
+    assert len(label_list) == len(output_list)
     match = 0
-    assert len(input_list) == len(output_list)
-    for i in range(len(input_list)):
-        if input_list[i] == output_list[i][0]:
-            match += 1
-    accuracy = match/len(input_list)
+    failed_match = 0
+    with open("../data/matches.txt", "w+") as f1, open("../data/failed_matches.txt", "w+") as f2:
+        f1.write("TOKEN\t\t" + "OUTPUT\t\t\t" + "CORRECT\n")
+        f2.write("TOKEN\t\t" + "OUTPUT\t\t\t" + "CORRECT\n")
+        for i in range(len(label_list)):
+            if label_list[i] == output_list[i][0]:
+                match += 1
+                f1.write(token_list[i] + "\t\t\t" + output_list[i][0] + " ({})".format(output_list[i][1]) + "\t\t\t" + label_list[i] + "\n")
+            else:
+                f2.write(token_list[i] + "\t\t\t" + output_list[i][0] + " ({})".format(output_list[i][1]) + "\t\t\t" + label_list[i] + "\n")
+                failed_match += 1
+
+
+    accuracy = match/len(token_list)
+    print("Matches: {}, Failed matches: {}".format(match, failed_match))
     print("Accuracy: {:.2%}".format(accuracy) + " ({})".format(accuracy))
     return
 
@@ -55,6 +69,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_path = args.input
     output_path = args.output
-    input = parse_input(input_path)
+    tokens, labels = parse_labels(input_path)
     output = parse_output(output_path)
-    evaluate(input, output)
+    evaluate(tokens, labels, output)
